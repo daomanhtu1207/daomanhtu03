905adcb0a886719c49531946035cff3a966724e9
daomanhtu1207 <118282642+daomanhtu1207@users.noreply.github.com>
2022-11-15 21:56:02 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+## <p align="center"> BY Ngô Văn Toàn </p>
+
+<hr>
+
---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Gửi Crush của tôi",
+    introTitle: 'Crush cutee',
+    introDesc: `Anh là một kẻ nhạt nhẽo
+    Anh không giỏi văn thơ
+    Giống như một kẻ khờ
+    Anh yêu em trong từng nhịp thở`,
+    btnIntro: '^^HiHiHi^^',
+    title: 'Mình yêu nhau đi cho thêm bận 🥰',
+    desc: 'Đừng thoát, hãy trả lời tôi !!!',
+    btnYes: 'Yêu luôn <33',
+    btnNo: 'Không yêu :3',
+    question: 'Nếu tình yêu như tờ vé số, anh mong 2 ta trúng số phu thê <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Không yêu cũng phải yêu đó <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Nhắn tin cho tôi luôn nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/NgoVanToan.TTB'
+}
